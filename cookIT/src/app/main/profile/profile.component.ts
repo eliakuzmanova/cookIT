dad7d6ca6e3f4ab646ff418163a326b94f19fb5c
Elia Kuzmanova <eliamitova7@gmail.com>
2023-07-21 21:22:14 +0200

    fix profile

--- cookIT/src/app/main/profile/profile.component.ts
+++ cookIT/src/app/main/profile/profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProfileService } from './profile.service';
-import {IUser} from "../../interfaces/index"
+import { IUser } from "../../interfaces/index"
 import { AuthService } from '../auth/auth.service';
 
 @Component({
@@ -9,35 +9,45 @@ import { AuthService } from '../auth/auth.service';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit{
+export class ProfileComponent implements OnInit {
   id!: string;
   user!: IUser | undefined;
   isPluralLength = false;
   hasRecipes = false;
-  loggedUserId!: IUser | undefined;
-  constructor(private profileService: ProfileService, private route: ActivatedRoute, private authService: AuthService){
+  loggedUser!: IUser | undefined;
+  constructor(private profileService: ProfileService, private route: ActivatedRoute, private authService: AuthService) {
 
   }
 
   ngOnInit(): void {
-    
-    if(this.authService.getUserInfo()) {
-      this.loggedUserId = this.authService.getUserInfo()._id
-      if(this.route.snapshot.params['id'] == this.loggedUserId){
-        this.user = this.loggedUserId
-      }
-    } else {
-      this.id =  this.route.snapshot.params['id']
-      this.profileService.getUserById(this.id).subscribe(value => {
-        this.user = value;       
-       })
-    }
+    this.loggedUser = this.authService.getUserInfo()
+
+
+    if (this.route.snapshot.params['id'] == this.loggedUser?._id) {
+      this.user = this.loggedUser
       this.user!.image = `http://localhost:5750/${this.user!.image}`
       this.user!.recipes.map((recipe) => {
-      recipe.image = `http://localhost:5750/${recipe.image}`;
-      this.isPluralLength = this.user!.recipes.length > 1 ? true : false;
-      this.hasRecipes = this.user!.recipes.length < 1 ? true : false;
-      
-     })
+        recipe.image = `http://localhost:5750/${recipe.image}`;
+        this.isPluralLength = this.user!.recipes.length > 1 ? true : false;
+        this.hasRecipes = this.user!.recipes.length < 1 ? true : false;
+
+      })
+
+    } else {
+      this.id = this.route.snapshot.params['id']
+      this.profileService.getUserById(this.id).subscribe(value => {
+        this.user = value;
+        this.user!.image = `http://localhost:5750/${this.user!.image}`
+        this.user!.recipes.map((recipe) => {
+          recipe.image = `http://localhost:5750/${recipe.image}`;
+          this.isPluralLength = this.user!.recipes.length > 1 ? true : false;
+          this.hasRecipes = this.user!.recipes.length < 1 ? true : false;
+
+        })
+      })
+
+
+    }
+
   }
 }
