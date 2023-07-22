223d3af1e5962f8bad3a7b4bcaf63ac45cfb23b2
Elia Kuzmanova <eliamitova7@gmail.com>
2023-07-22 19:32:21 +0200

    add edit profile view and some functionality

--- cookIT/src/app/main/edit-profile/edit-profile.component.ts
+++ cookIT/src/app/main/edit-profile/edit-profile.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
+import { NgForm} from '@angular/forms';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
+import { IUser } from 'src/app/interfaces';
 
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
   styleUrls: ['./edit-profile.component.css']
 })
-export class EditProfileComponent {
+export class EditProfileComponent implements OnInit {
 
+  @ViewChild("form") form!: NgForm;
+
+  image: File | undefined;
+  user!: IUser;
+  usernameValue!: string;
+  selectedImage: string | undefined;
+
+constructor(private authService: AuthService ,private router: Router) {}
+
+  ngOnInit(): void {
+    this.user = this.authService.getUserInfo()
+    this.user.image = `http://localhost:5750/${this.user.image}`
+    this.usernameValue = this.user.username
+  }
+
+
+  OnFileChange(e: any) {
+    this.image = e.target.files[0]
+    if(e.target.files[0]) {
+    this.selectedImage = e.target.files[0].name;
+    } else {
+      this.selectedImage = undefined;
+    }
+    
+  }
+
+  onSubmit(form: NgForm): void {
+    
+  }
 }
