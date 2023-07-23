70e172553889be7faf3a436f8ac183b0c3ab9258
Elia Kuzmanova <eliamitova7@gmail.com>
2023-07-23 16:30:37 +0200

    add edit recipe in details

--- cookIT/src/app/main/details/details.component.ts
+++ cookIT/src/app/main/details/details.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { DetailsService } from './details.service';
 import { ActivatedRoute } from '@angular/router';
 import { IRecipe } from "../../interfaces/index"
-
+import { IUser } from 'src/app/interfaces';
+import { AuthService } from '../auth/auth.service';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
@@ -13,17 +14,23 @@ export class DetailsComponent implements OnInit {
   isPluralLength = false;
   recipeId!: string;
   recipe: IRecipe | undefined;
-  constructor(private route: ActivatedRoute, private detailService: DetailsService) { 
+  loggedUser : IUser | undefined;
+  isAuthor: boolean = false;
 
-  }
-  ngOnInit() {
-
+  constructor(private route: ActivatedRoute, private detailService: DetailsService, private authService: AuthService,) { }
+  ngOnInit() {  
     this.recipeId = this.route.snapshot.params['id']
     this.detailService.getDetails(this.recipeId).subscribe(value => {
     this.recipe = value;
     this.recipe.image = `http://localhost:5750/${this.recipe.image}`
     this.recipe.author.image = `http://localhost:5750/${this.recipe.author.image}`
     this.isPluralLength = this.recipe.author.recipes.length > 1 ? true : false;
+    this.loggedUser = this.authService.getUserInfo();
+    this.isAuthor = this.loggedUser?._id == this.recipe?.author._id
+    console.log(this.isAuthor);
+    
     })
+  
+    
   }
 }
